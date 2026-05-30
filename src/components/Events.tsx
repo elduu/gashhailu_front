82f3bb43d956b325c36e71e94eba3147587ec7aa
redietyilma <119247439+elduu@users.noreply.github.com>
2026-05-30 09:36:13 +0300

    cors

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -18,16 +18,16 @@ const Events = () => {
 
         {/* Google Map */}
         <div className="rounded-xl overflow-hidden shadow-lg border border-secondary/20">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5!2d38.7578!3d9.0192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef5ab402d%3A0x8467b6b037a24d49!2sAddis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2sus!4v1600000000000"
-            width="100%"
-            height="350"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            title="Wedding Location"
-          />
+       <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.226083559722!2d38.9427656!3d8.764787499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b72d3c27674db%3A0x989f05740e4beeb1!2sQW7V%2BW46%2C%20Bishoftu!5e0!3m2!1sen!2set!4v1780122806835!5m2!1sen!2set"
+    width="100%"
+    height="350"
+    style={{ border: 0 }}
+    allowFullScreen
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    title="Hotel Location"
+  />
         </div>
 
         {/* Location Buttons */}
