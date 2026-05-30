2de7f54c2b36f502042c756452d647e0cf39ae60
redietyilma <119247439+elduu@users.noreply.github.com>
2026-05-30 09:54:19 +0300

    ydhd

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -5,7 +5,7 @@ const locationButtons = [
   {
     label: "Hotel",
     icon: "🏡",
-    mapUrl: "https://maps.app.goo.gl/inCtnhH5TTbEwRoh8?g_st=atm",
+    mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.226083559722!2d38.9427656!3d8.764787499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b72d3c27674db%3A0x989f05740e4beeb1!2sQW7V%2BW46%2C%20Bishoftu!5e0!3m2!1sen!2set!4v1780122806835!5m2!1sen!2set",
   },
   
  
@@ -20,7 +20,6 @@ const Events = () => {
         <div className="rounded-xl overflow-hidden shadow-lg border border-secondary/20">
        <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.226083559722!2d38.9427656!3d8.764787499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b72d3c27674db%3A0x989f05740e4beeb1!2sQW7V%2BW46%2C%20Bishoftu!5e0!3m2!1sen!2set!4v1780122806835!5m2!1sen!2set"
-    width="100%"
     height="350"
     style={{ border: 0 }}
     allowFullScreen
